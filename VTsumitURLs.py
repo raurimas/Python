1e2d246a101063f05f96b75621c5e4ed951b3976
raurimas <arudinskis@gmail.com>
2016-01-05 09:50:44 +0200

    Minor changes

--- VTsumitURLs.py
+++ VTsumitURLs.py
@@ -1,15 +1,18 @@
-import requests, time
+import time, urllib.request, urllib.parse
+#from urllib.parse import urlencode
 
-log_file = 'C:\Users\304452\Desktop\phising_patterns2.txt'
+log_file = "C:\\Users\\304452\\Desktop\\phising_patterns2.txt"
 with open(log_file) as f:
     urls = f.readlines()
 
 for url in urls:
-    print(url)
+    vturl = "https://www.virustotal.com/vtapi/v2/url/scan"
     params = {'url': url, 'apikey': '23c479aa8d758b623162984a38894ed305591648440b7439d0632dce1a2e96b5'}
-    response = requests.get('https://www.virustotal.com/vtapi/v2/url/scan', params=params)
-    json_response = response.json()
-    print(json_response)
+    data = urllib.parse.urlencode(params).encode('utf8')
+    response = urllib.request.urlopen(vturl, data)
+    response_bytes = response.read()
+    response = response_bytes.decode('utf8')
+    print(response)
 
     # Public API allows to scan 4 files per minute
     time.sleep(15)
