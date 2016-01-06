73f181062ae376d10b7c8dd048465566fc7d006d
raurimas <arudinskis@gmail.com>
2016-01-06 11:50:54 +0200

    Minor changes

--- SubmitURLvoid.py
+++ SubmitURLvoid.py
@@ -1,13 +1,23 @@
-import time, urllib.request, urllib.parse, json
+import urllib.request, urllib.parse
+from xml.etree import ElementTree
 
 log_file = "C:\\Users\\304452\\Desktop\\phising_patterns2.txt"
 with open(log_file) as f:
     urls = f.readlines()
 
 def urlreport(url):
-    vturl = 'https://www.virustotal.com/vtapi/v2/url/report'
-    params = {'resource': url, 'apikey': '23c479aa8d758b623162984a38894ed305591648440b7439d0632dce1a2e96b5'}
-    data = urllib.parse.urlencode(params).encode('utf8')
-    response = urllib.request.urlopen(vturl, data)
-    data = json.loads(response.read().decode('utf8'))
-    return(data)
+    urlvoid = 'http://api.urlvoid.com/api1000/6a80bc6bbb0e7920c00dff9cc909344f29038736/host/'
+    response = urllib.request.urlopen(urlvoid + url.strip('\n') + '/scan/')
+    data = response.read().decode('utf8')
+    tree = ElementTree.fromstring(data)
+    try:
+        return tree.find('action_result').text
+    except Exception:
+        pass
+
+for url in urls:
+    urlstatus = urlreport(url)
+    if urlstatus != None:
+        print(url.strip('\n'), urlstatus)
+    else:
+        print(url.strip('\n'), 'Domain not valid or error occured')
